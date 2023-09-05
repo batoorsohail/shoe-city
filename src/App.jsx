fb0dc3f726b114815d61243528c353e74d0f1289
unknown <sohailshahidi1435@gmail.com>
2023-09-05 22:23:34 +0430

    Setup the App file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
-function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+const App = () => (
+  <main className="relative">
+    Nav
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      Hero
+    </section>
+    <section className="padding">
+      PopularProducts
+    </section>
+    <section className="padding">
+      SuperQuality
+    </section>
+    <section className="padding-x p-y-10">
+      Services
+    </section>
+    <section className="padding">
+      SpecialOffers
+    </section>
+    <section className="bg-pale-blue padding">
+      CustomerReviews
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      Subscribe
+    </section>
+    <section className="bg-black padding-x padding-t pb-8">
+      Footer
+    </section>
+  </main>
+)
 
 export default App
