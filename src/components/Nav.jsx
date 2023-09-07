90f043f69b6299bf5fa79006e04bcb9935ae3cec
unknown <sohailshahidi1435@gmail.com>
2023-09-07 22:17:36 +0430

    Add Navbar file

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,8 +3,8 @@ import { hamburger } from '../assets/icons';
 
 const Nav = () => {
   return (
-    <header>
-      <nav>
+    <header className="padding-x py-8 absolute z-10 w-full">
+      <nav className="flex justify-between items-center max-container">
         <a href="/">
           <img 
             src={headerLogo} 
