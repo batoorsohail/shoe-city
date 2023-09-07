be39a4dec9f9a8761a2f82fe56bbd418aca8d137
unknown <sohailshahidi1435@gmail.com>
2023-09-07 22:33:16 +0430

    Add Navigation Section

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,6 @@
 import { headerLogo } from '../assets/images';
 import { hamburger } from '../assets/icons';
+import { navLinks } from '../constants';
 
 const Nav = () => {
   return (
@@ -13,6 +14,23 @@ const Nav = () => {
             height={29}
           />
         </a>
+        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+          {navLinks.map((item) => 
+            <li key={item.label}>
+              <a href={item.href} className="font-montserrat leading-normal text-lg text-slate-gray">
+                {item.label}
+              </a>
+            </li>
+          )}
+        </ul>
+        <div className="hidden max-lg:block">
+          <img 
+            src={hamburger}
+            alt="hamburger"
+            width={25}
+            height={25}
+          />
+        </div>
       </nav>
     </header>
   )
