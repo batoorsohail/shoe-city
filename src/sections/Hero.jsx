ffd0cbce61f2681af5dacb4c0cbe13e7c681c9c3
unknown <sohailshahidi1435@gmail.com>
2023-09-09 22:16:58 +0430

    Add Hero section texts

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,16 @@
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container">
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
+        <p>Our Summer Collections</p>
+        <h1>
+          <span>The New Arrival</span>
+          <span>Nike</span>
+          Shoes
+        </h1>
+        <p>Discover stylish Nike arrivals, quality comfort, and innovation for your active life.</p>
+      </div>
+    </section>
   )
 }
 
