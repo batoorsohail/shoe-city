3303092c7be3028db0ce55997fe097b058abfe75
unknown <sohailshahidi1435@gmail.com>
2023-09-09 22:37:45 +0430

    Add statistics section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,6 @@
 import { arrowRight } from "../assets/icons"
 import Button from "../components/Button"
+import { statistics } from "../constants"
 
 const Hero = () => {
   return (
@@ -13,6 +14,14 @@ const Hero = () => {
         </h1>
         <p>Discover stylish Nike arrivals, quality comfort, and innovation for your active life.</p>
         <Button label={"Shop Now"} iconURL={arrowRight} />
+        <div className="flex justify-start items-center flex-wrap w-full mt-20 gap-16">
+          {statistics.map((stat) => (
+            <div key={stat.label}>
+              <p>{stat.value}</p>
+              <p>{stat.label}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   )
