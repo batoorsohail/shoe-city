58eab20dbef939ac6cb0d09a77c7f6257d7e14d0
unknown <sohailshahidi1435@gmail.com>
2023-09-09 23:18:37 +0430

    Add Shoe collection sections

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,4 +1,5 @@
 import { arrowRight } from "../assets/icons"
+import { bigShoe1 } from "../assets/images"
 import Button from "../components/Button"
 import { statistics } from "../constants"
 
@@ -22,6 +23,15 @@ const Hero = () => {
             </div>
           ))}
         </div>
+      </div>
+      <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
+        <img
+          src={ bigShoe1 }
+          alt="Big shoe"
+          width={610}
+          height={500}
+          className="object-contain relative z-0"
+        />
       </div>
     </section>
   )
