e515242d3866fd2eab46c5e87dc84be8c864db97
unknown <sohailshahidi1435@gmail.com>
2023-09-10 22:37:42 +0430

    Add ShowCard component styles

--- src/components/ShowCard.jsx
+++ src/components/ShowCard.jsx
@@ -1,6 +1,18 @@
-const ShowCard = () => {
+const ShowCard = ({imgURL, changeBigShoeImg, bigShoeImg}) => {
+  const handleClick = (() => {
+    if (bigShoeImg !== imgURL.bigShoe){
+      changeBigShoeImg(imgURL.bigShoe);
+    }
+  })
+
   return (
-    <div>ShowCard</div>
+    <div className={`border-2 rounded-xl ${bigShoeImg === imgURL ? 'border-coral-red' 
+      : 'border-transparent'} cursor-pointer max-sm:flex-1`} onClick={handleClick}
+    >
+      <div className="flex justify-center items-center bg-card bg-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:p-4">
+        <img src={imgURL.thumbnail} alt="shoes collection" width={127} height={103} className="object-contain" />
+      </div>
+    </div>
   )
 }
 
