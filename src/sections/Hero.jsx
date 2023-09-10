fa4fbaaee8a6d8f12141e645219723226cf717a7
unknown <sohailshahidi1435@gmail.com>
2023-09-10 21:43:26 +0430

    Fix Home section style issues

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -8,10 +8,10 @@ const Hero = () => {
     <section className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container">
       <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
         <p className="text-xl font-montserrat text-coral-red">Our Summer Collections</p>
-        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold">
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
           <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">The New Arrival</span>
           <br />
-          <span className="text-coral-red inline-block mt-10">Nike</span> Shoes
+          <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
         </h1>
         <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-16 sm:max-w-sm">Discover stylish Nike arrivals, quality comfort, and innovation for your active life.</p>
         <Button label={"Shop Now"} iconURL={arrowRight} />
