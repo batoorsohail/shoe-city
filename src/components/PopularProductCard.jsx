30841349ec4e66f24a730b0a9a8ebe599ce80ae9
unknown <sohailshahidi1435@gmail.com>
2023-09-12 22:50:38 +0430

    Add PopularProduct images

---
+++ src/components/PopularProductCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PopularProductCard = ({imgURL, name, price}) => {
+  return (
+    <div className='flex flex-1 flex-col w-full max-sm:w-full'>
+      <img src={imgURL} alt={name} className='w-[280px] h-[280px]' />
+    </div>
+  )
+}
+
+export default PopularProductCard
