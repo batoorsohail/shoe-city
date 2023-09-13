0fe8b8e6d0f4ce446223c26fddc52cb115b23d95
unknown <sohailshahidi1435@gmail.com>
2023-09-13 18:54:22 +0430

    Add SuperQuality styles

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ const Button = ({label, iconURL}) => {
   return (
     <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
       {label}
-      <img src={iconURL} alt="icon arrow right" />
+      {iconURL && <img src={iconURL} alt="icon arrow right" />}
     </button>
   )
 }
--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,16 +1,22 @@
+import { arrowRight } from "../assets/icons"
+import { shoe8 } from "../assets/images"
 import Button from "../components/Button"
 
 const SuperQuality = () => {
   return (
     <section id="about-us" className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container">
       <div className="flex flex-1 flex-col">
-        <h2 className="mt-10 font-palanquin text-4xl capitalize font-bold">
-          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">The New Arrival</span>
-          <br />
-          <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
+        <h2 className="mt-10 font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+          We provide you <span className="text-coral-red inline-block">Super</span> <span className="text-coral-red inline-block">Quality</span>
         </h2>
-        <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-16 sm:max-w-sm">Discover stylish Nike arrivals, quality comfort, and innovation for your active life.</p>
-        <Button label={"View details"} />
+        <p className="mt-4 lg:max-w-lg info-text">Ensuring premium comfort and style, our meticulously crafted footwear is designed to elevate your experience, providing you with unmatched quality, innovation, and a touch of elegance.</p>
+        <p className="mt-6 lg:max-w-lg info-text">Our dedication to detail and excellence ensures your satisfaction</p>
+        <div className="mt-11">
+          <Button label={"View details"}/>
+        </div>
+      </div>
+      <div className="flex-1 flex justify-center items-center">
+        <img src={shoe8} alt="Shoe8" width={570} height={522} className="object-contain" />
       </div>
     </section>
   )
