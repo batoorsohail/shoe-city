8bf8b1a9f30a1b62f74b75cc8f93db84f14d7065
unknown <sohailshahidi1435@gmail.com>
2023-09-13 19:05:54 +0430

    Add ServiceCard.jsx file

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,6 +1,13 @@
+import { services } from "../constants"
+import ServiceCard from "../components/ServiceCard"
+
 const Services = () => {
   return (
-    <div>Services</div>
+    <section className="max-container flex justify-center gap-9">
+      {services.map((service) => (
+        <ServiceCard key={service.label} {...service} />
+      ))}
+    </section>
   )
 }
 
