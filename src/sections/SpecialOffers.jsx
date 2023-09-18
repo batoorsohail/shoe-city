eb44c8d25328144916c305db00c545e1a14890fe
unknown <sohailshahidi1435@gmail.com>
2023-09-18 17:35:25 +0430

    Add SpecialOffers section

--- src/sections/SpecialOffers.jsx
+++ src/sections/SpecialOffers.jsx
@@ -1,6 +1,15 @@
 const SpecialOffers = () => {
   return (
-    <div>SpecialOffers</div>
+    <div className="flex flex-1 flex-col">
+        <h2 className="mt-10 font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+          We provide you <span className="text-coral-red inline-block">Super</span> <span className="text-coral-red inline-block">Quality</span>
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text">Ensuring premium comfort and style, our meticulously crafted footwear is designed to elevate your experience, providing you with unmatched quality, innovation, and a touch of elegance.</p>
+        <p className="mt-6 lg:max-w-lg info-text">Our dedication to detail and excellence ensures your satisfaction</p>
+        <div className="mt-11">
+          <Button label={"View details"}/>
+        </div>
+    </div>
   )
 }
 
