e2298823f4aa13fb81732016b7d93ca8896c2301
unknown <sohailshahidi1435@gmail.com>
2023-09-20 20:03:10 +0430

    Update CustomerReviews section

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,6 +1,18 @@
+import { reviews } from "../constants"
+
 const CustomerReviews = () => {
   return (
-    <div>CustomerReviews</div>
+    <section className="max-container">
+      <h3 className="font-palanquin text-center text-4xl font-bold">
+        What Ours? <span className="text-coral-red">Customers</span> Say?
+      </h3>
+      <p className="info-text m-auto mt-4 max-w-lg text-center">Hear genuine stories from our satisfied customers about their exceptional experiences with us.</p>
+      <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
+        {reviews.map((review) => (
+          <ReviewCard />
+        ))}
+      </div>
+    </section>
   )
 }
 
