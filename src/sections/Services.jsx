79eb72498ecec77c45f4111e78f1eb5203bcdad0
unknown <sohailshahidi1435@gmail.com>
2023-09-20 22:21:32 +0430

    Update stylings

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -3,7 +3,7 @@ import ServiceCard from "../components/ServiceCard"
 
 const Services = () => {
   return (
-    <section className="max-container flex justify-center gap-9">
+    <section className="max-container flex justify-center flex-wrap gap-9">
       {services.map((service) => (
         <ServiceCard key={service.label} {...service} />
       ))}
