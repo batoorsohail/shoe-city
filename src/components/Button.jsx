06658feadf11f37a13c499a9007104fccacbab96
unknown <sohailshahidi1435@gmail.com>
2023-09-20 22:20:10 +0430

    Update Buttons components

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,8 @@
-const Button = ({label, iconURL, backgroundColor, borderColor, textColor}) => {
+const Button = ({label, iconURL, backgroundColor, borderColor, textColor, fullWidth}) => {
   return (
     <button className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
       ${backgroundColor ? `${backgroundColor} ${borderColor} ${textColor}`
-      : "bg-coral-red text-white border-coral-red"} rounded-full w-full"}`}
+      : "bg-coral-red text-white border-coral-red"} rounded-full ${fullWidth && 'w-full'}"}`}
     >
       {label}
       {iconURL && <img src={iconURL} alt="icon arrow right" />}
