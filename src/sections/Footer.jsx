46ca4fa45f1b499b131970c31ecbca354162c1e2
unknown <sohailshahidi1435@gmail.com>
2023-09-21 22:17:32 +0430

    Add Footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,6 +1,47 @@
+import footerLogo from "../assets/images/footer-logo.png"
+import { footerLinks, socialMedia } from "../constants"
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="max-container">
+      <div className="flex justify-between items-start flex-wrap max-lg:flex-col">
+        <div className="flex flex-col items-start">
+          <a href="/">
+            <img src={footerLogo} alt="logo" width={150} height={46} />
+          </a>
+          <p className="mt-6 text-base leading-7 font-montserrat text-white-400 sm:max-w-sm">
+            Get shoes ready for the new term at your nearest Nike store. 
+            Find Your perfect Size In Store. Get Rewards
+          </p>
+          <div className="flex items-center gap-5 mt-8">
+            {socialMedia.map((icon) => {
+              <div className="flex justify-center items-center bg-white rounded-full w-12 h-12" key={icon}>
+                <img src={icon.src} alt={icon.alt} width={24} height={24} />
+              </div>
+            })}
+          </div>
+          <div className="flex flex-1 justify-between lg:gap-10 gap-20 flex-wrap">
+            {footerLinks.map((section) => {
+              <div key={section}>
+                <h4 className="text-white font-montserrat text-2xl leading-normal font-medium mb-6">
+                  {section.title}
+                </h4>
+                <ul>
+                  {section.links.map((link) => {
+                    <li 
+                      className="mt-3 text-white-400 font-montserrat text-base leading-normal hover:text-slate-gray cursor-pointer"
+                      key={link.name}
+                    >
+                        <a href={link.name}></a>
+                    </li>
+                  })}
+                </ul>
+              </div>
+            })}
+          </div>
+        </div>
+      </div>
+    </footer>
   )
 }
 
