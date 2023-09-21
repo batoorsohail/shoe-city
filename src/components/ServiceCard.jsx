f3e4668cdf2f9f94fb81da4d59757c5827b322fd
unknown <sohailshahidi1435@gmail.com>
2023-09-21 22:59:41 +0430

    Fix Linters error

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const ServiceCard = ({imgURL, label, subtext}) => {
   return (
     <div className="flex-1 sm:w-[350px] sm:min-w-[350px] w-full rounded-[20px] shadow-3xl px-10 py-16">
