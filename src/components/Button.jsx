4a8cc3d3930a5b82a6928960e7c0bffe045a20fe
unknown <sohailshahidi1435@gmail.com>
2023-09-23 23:07:29 +0430

    Add Animation and Transtion for buttons

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,10 +2,10 @@ const Button = ({label, iconURL, backgroundColor, borderColor, textColor, fullWi
   return (
     <button className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
       ${backgroundColor ? `${backgroundColor} ${borderColor} ${textColor}`
-      : "bg-coral-red text-white border-coral-red"} rounded-full ${fullWidth && 'w-full'}"}`}
+      : "bg-coral-red text-white border-coral-red"} rounded-full ${fullWidth && 'w-full'}"} hover:text-coral-red hover:bg-white hover:border-coral-red duration-500 ease-in-out`}
     >
       {label}
-      {iconURL && <img src={iconURL} alt="icon arrow right" />}
+      {iconURL && <img src={iconURL} alt="icon arrow right" className="hover:translate-x-1.5 duration-500 ease-in-out"/>}
     </button>
   )
 }
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = () => {
           <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
         </h1>
         <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-16 sm:max-w-sm">Discover stylish Nike arrivals, quality comfort, and innovation for your active life.</p>
-        <Button label={"Shop Now"} iconURL={arrowRight} />
+        <Button label={"Shop Now"} iconURL={arrowRight} className="hover:bg-white hover:border-coral-red duration-500 ease-in-out" />
         <div className="flex justify-start items-center flex-wrap w-full mt-20 gap-16">
           {statistics.map((stat) => (
             <div key={stat.label}>
