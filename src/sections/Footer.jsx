62778bbfae82d955bbf8431cab6cf22545195588
unknown <sohailshahidi1435@gmail.com>
2023-09-23 23:48:48 +0430

    Add hover effect for social links

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -23,7 +23,7 @@ const Footer = () => {
           <div className='flex items-center gap-5 mt-8'>
             {socialMedia.map((icon) => (
               <div
-                className='flex justify-center items-center w-12 h-12 bg-white rounded-full'
+                className='flex justify-center items-center w-12 h-12 bg-white rounded-full hover:translate-y-[-5px] duration-150'
                 key={icon.alt}
               >
                 <img src={icon.src} alt={icon.alt} width={24} height={24} />
